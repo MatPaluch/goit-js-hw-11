86364abe98f263e2fc7f608f1725617b177e69b7
Mateusz Paluszkiewicz <150561295+MatPaluch@users.noreply.github.com>
2024-04-28 14:25:27 +0200

    Update requests.js

--- src/javascript/requests.js
+++ src/javascript/requests.js
@@ -1,24 +1,9 @@
-const headers = new Headers({
-  'Content-Type': 'application/json',
-  'x-api-key': '43605256-eead80bfe3e75f279f48bfba2',
-});
-
-var requestOptions = {
-  method: 'GET',
-  headers: headers,
-  redirect: 'follow',
-};
-
 export async function searchPhrase(text) {
   const searchParams = new URLSearchParams({
     key: '43605256-eead80bfe3e75f279f48bfba2',
     q: text,
     image_type: 'photo',
   });
-  console.log(searchParams);
 
-  return await fetch(
-    `https://pixabay.com/api/?${searchParams}`,
-    requestOptions
-  );
+  return await fetch(`https://pixabay.com/api/?${searchParams}`);
 }
