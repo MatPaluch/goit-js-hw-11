81c2ca920dee5cdb841447424bdb5aff83ba3df6
Mateusz Paluszkiewicz <150561295+MatPaluch@users.noreply.github.com>
2024-04-28 16:58:38 +0200

    Update requests.js

--- src/javascript/requests.js
+++ src/javascript/requests.js
@@ -3,6 +3,10 @@ export async function searchPhrase(text) {
     key: '43605256-eead80bfe3e75f279f48bfba2',
     q: text,
     image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: false, 
+    page: 1, 
+    per_page: 40
   });
 
   return await fetch(`https://pixabay.com/api/?${searchParams}`);
