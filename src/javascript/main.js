d3e6092172369759206ec8948a866cfe6c0ecdb7
MatPaluch <xp.mateusz@gmail.com>
2024-04-29 00:32:25 +0200

    Finito

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,16 +1,16 @@
 const axios = require('axios').default;
 import Notiflix from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
-var lightbox = new SimpleLightbox('.gallery a', {
-  /* options */
-});
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { searchPhrase } from './requests';
 
 const inputForm = document.querySelector('#search-form input');
-const buttonForm = document.querySelector('#search-form button');
 const searchForm = document.querySelector('#search-form');
+const loadMore = document.querySelector('.load_more');
 
 const gallery = document.querySelector('.gallery');
+let page = 1;
 
 searchForm.addEventListener('submit', ev => {
   ev.preventDefault();
@@ -18,28 +18,77 @@ searchForm.addEventListener('submit', ev => {
   const form = ev.target;
   const input = form.elements.searchQuery;
 
-  searchPhrase(input.value)
-    .then(response => response.json())
-    .then(result => {
-      const arrayOfHits = result.hits;
-      arrayOfHits.map(obj => {
-        gallery.innerHTML += `<div class="photo-card"><img src="${obj.webformatURL}" alt="${obj.tags}" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes ${obj.likes}</b>
-    </p>
-    <p class="info-item">
-      <b>Views ${obj.views}</b>
-    </p>
-    <p class="info-item">
-      <b>Comments ${obj.comments}</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads ${obj.downloads}</b>
-    </p>
-  </div>
-</div>`;
-      });
-    })
-    .catch(error => console.log(error));
+  if (input.value === '') {
+    Notiflix.Notify.failure('Type some words in search box ;)');
+  } else {
+    searchPhrase(input.value)
+      .then(response => response.json())
+      .then(result => {
+        const arrayOfHits = result.hits;
+        console.log(arrayOfHits);
+        if (arrayOfHits.length === 0) {
+          Notiflix.Notify.failure(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
+        } else {
+          Notiflix.Notify.success(
+            `Hooray! We found ${result.totalHits} images.`
+          );
+          arrayOfHits.map(obj => {
+            gallery.innerHTML += `<div class="photo-card">
+          <a href="${obj.largeImageURL}"><img src="${obj.webformatURL}" alt="${obj.tags}" title="User: ${obj.user}" loading="lazy" /></a>
+          <div class="info">
+            <p class="info-item"><b>Likes</b><span>${obj.likes}</span></p>
+            <p class="info-item"><b>Views</b><span>${obj.views}</span></p>
+            <p class="info-item"><b>Comments</b><span>${obj.comments}</span></p>
+            <p class="info-item"><b>Downloads</b><span>${obj.downloads}</span></p>
+          </div>
+        </div>`;
+          });
+          loadMore.removeAttribute('hidden');
+        }
+        var lightbox = new SimpleLightbox('.gallery a', {
+          /* options */
+        });
+      })
+      .catch(error => console.log(error));
+  }
+});
+
+loadMore.addEventListener('click', ev => {
+  page++;
+  if (inputForm.value === '') {
+    Notiflix.Notify.failure('Type some words in search box ;)');
+  } else {
+    searchPhrase(inputForm.value, page)
+      .then(response => response.json())
+      .then(result => {
+        const arrayOfHits = result.hits;
+        console.log(arrayOfHits);
+        if (arrayOfHits.length === 0) {
+          Notiflix.Notify.failure(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
+        } else {
+          Notiflix.Notify.success(
+            `Hooray! We found ${result.totalHits} images.`
+          );
+          arrayOfHits.map(obj => {
+            gallery.innerHTML += `<div class="photo-card">
+          <a href="${obj.largeImageURL}"><img src="${obj.webformatURL}" alt="${obj.tags}" title="User:${obj.user}" loading="lazy" /></a>
+          <div class="info">
+            <p class="info-item"><b>Likes ${obj.likes}</b></p>
+            <p class="info-item"><b>Views ${obj.views}</b></p>
+            <p class="info-item"><b>Comments ${obj.comments}</b></p>
+            <p class="info-item"><b>Downloads ${obj.downloads}</b></p>
+          </div>
+        </div>`;
+          });
+        }
+        var lightbox = new SimpleLightbox('.gallery a', {
+          /* options */
+        });
+      })
+      .catch(error => console.log(error));
+  }
 });
--- src/javascript/requests.js
+++ src/javascript/requests.js
@@ -1,13 +1,13 @@
-export async function searchPhrase(text) {
+export async function searchPhrase(text, pageIndex) {
   const searchParams = new URLSearchParams({
     key: '43605256-eead80bfe3e75f279f48bfba2',
     q: text,
     image_type: 'photo',
     orientation: 'horizontal',
-    safesearch: false, 
-    page: 1, 
-    per_page: 40
+    safesearch: false,
+    lang: 'pl,en',
+    page: pageIndex,
+    per_page: 40,
   });
-
   return await fetch(`https://pixabay.com/api/?${searchParams}`);
 }
