96ab7df2e0459eddb35ba0b8b3b1193fd0e32f92
Mateusz Paluszkiewicz <150561295+MatPaluch@users.noreply.github.com>
2024-04-28 15:23:05 +0200

    Update main.js

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -10,6 +10,8 @@ const inputForm = document.querySelector('#search-form input');
 const buttonForm = document.querySelector('#search-form button');
 const searchForm = document.querySelector('#search-form');
 
+const gallery = document.querySelector('.gallery');
+
 searchForm.addEventListener('submit', ev => {
   ev.preventDefault();
 
@@ -19,7 +21,25 @@ searchForm.addEventListener('submit', ev => {
   searchPhrase(input.value)
     .then(response => response.json())
     .then(result => {
-      console.log(result);
+      const arrayOfHits = result.hits;
+      arrayOfHits.map(obj => {
+        gallery.innerHTML = `<div class="photo-card"><img src="${obj.webformatURL}" alt="" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+    </p>
+  </div>
+</div>`;
+      });
     })
     .catch(error => console.log(error));
 });
